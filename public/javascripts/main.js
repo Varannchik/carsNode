24ba9244187cb9b9c043bb31f5141088e225fe8b
varannchik <varannchik@gmail.com>
2019-08-19 23:22:38 +0300

    com

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,29 +1,19 @@
-const xhr = document.getElementById("submit");
+const btn = document.getElementById("submit");
 
-xhr.addEventListener('click',(e) => {
+btn.addEventListener('click',(e) => {
     e.preventDefault();    
     
     let model= document.getElementById("model").value;
     let price = document.getElementById("price").value;
     let picture = document.getElementById("picture").value;
-    console.log(model, price, picture);
     const car = JSON.stringify( {model: model, price: price, picture: picture});
+    console.log(car);
 
-    const request = new XMLHttpRequest();      
-    
-    request.addEventListener("load", function () {
-        request.open("POST", "/car");
-        request.setRequestHeader("Content-Type", "application/json");
-        console.log('1');
-        request.send(JSON.stringify({ model: model }));
-        response.send(`${request.body.model} - ${request.body.price}`);
-         
-    });
-    request.send();
+    const xhr = new XMLHttpRequest(); 
+    xhr.open("POST", "/", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+   
+    xhr.send(car);    
     
 })
-
-
-
-
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+let car = []
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Enter your car' });
 });
+
+router.post('/', function(req) {  
+  car.push(req.body);
+  console.log(car);   
+});
+
+
 
 module.exports = router;
